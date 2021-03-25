157c8cf83ed7b71c8188dcc0e999bc953184868c
Rinat Kamaletdinov <sky2208@ya.ru>
2021-03-25 21:07:47 +0400

    FIX

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 export default class ErrorRepository {
-    constructor() {
-        this.error = new Map();
-        this.error.set(404, "not found")
-                .set(300, "The connection is lost")
-                .set(400, "I cannot")
-    }
+  constructor() {
+    this.error = new Map();
+    this.error.set(404, 'not found')
+      .set(300, 'The connection is lost')
+      .set(400, 'I cannot');
+  }
 
-    translate(code) {
-        if(this.error.has(code)) {
-            return this.error.get(code)
-        } else {
-            return 'Unknown error';
-        }
+  translate(code) {
+    if (this.error.has(code)) {
+      return this.error.get(code);
     }
+    return 'Unknown error';
+  }
 }
