70e86859cf2de907084d15b6c16fcd19633c13d0
Rinat Kamaletdinov <sky2208@ya.ru>
2021-03-25 17:24:48 +0400

    Done

---
+++ src/index.js
@@ -0,0 +1,16 @@
+export default class ErrorRepository {
+    constructor() {
+        this.error = new Map();
+        this.error.set(404, "not found")
+                .set(300, "The connection is lost")
+                .set(400, "I cannot")
+    }
+
+    translate(code) {
+        if(this.error.has(code)) {
+            return this.error.get(code)
+        } else {
+            return 'Unknown error';
+        }
+    }
+}
---
+++ src/index.test.js
@@ -0,0 +1,11 @@
+import ErrorRepository from "./index";
+
+test("test", ()=> {
+    const error = new ErrorRepository();
+    expect(error.translate(300)).toEqual("The connection is lost")
+});
+
+test("test", ()=> {
+    const error = new ErrorRepository();
+    expect(error.translate(200)).toEqual('Unknown error');
+});
